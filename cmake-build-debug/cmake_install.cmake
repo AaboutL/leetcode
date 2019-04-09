a23adf018f22e6a850434906dd1b5203bee60375
Your Name <you@example.com>
2019-04-10 00:15:37 +0800

    add 0015

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/slam/leetcode
+# Install script for directory: /home/aal/workspace/leetcode
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,7 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/slam/leetcode/cmake-build-debug/samples/cmake_install.cmake")
+  include("/home/aal/workspace/leetcode/cmake-build-debug/samples/cmake_install.cmake")
 
 endif()
 
@@ -51,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/slam/leetcode/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/aal/workspace/leetcode/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- cmake-build-debug/samples/CMakeFiles/main.dir/DependInfo.cmake
+++ cmake-build-debug/samples/CMakeFiles/main.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/slam/leetcode/samples/main.cc" "/home/slam/leetcode/cmake-build-debug/samples/CMakeFiles/main.dir/main.cc.o"
+  "/home/aal/workspace/leetcode/samples/main.cc" "/home/aal/workspace/leetcode/cmake-build-debug/samples/CMakeFiles/main.dir/main.cc.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
